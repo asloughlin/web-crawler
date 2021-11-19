832a74539ab1cc3e6de01cebc00f37a2f2b2ed2a
ronanloughlin <loughlin.r@northeastern.edu>
2021-11-19 09:03:09 -0500

    finish assignment

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use std::io::BufReader;
 use std::str::from_utf8;
 use regex::Regex;
 use select::document::Document;
-use select::predicate::{Name, Predicate};
+use select::predicate::{Class, Name, Predicate};
 use std::collections::{HashMap, HashSet};
 
 extern crate queues;
@@ -24,6 +24,7 @@ fn read_http_response(mut stream: &mut TlsStream<TcpStream>, cookies: &mut HashM
     let mut reader = BufReader::new(&mut stream);
     let mut content_length = 0usize;
     let mut more_header = true;
+    let mut close_connection = false;
     let content_length_re = Regex::new(r"Content-Length: (?P<length>\d*)").unwrap();
     let set_cookie_re = Regex::new(r"Set-Cookie: (?P<name>[^=]*)=(?P<value>[^;]*);").unwrap();
     let found_url_re = Regex::new(r"Location: (?P<url>[^ ]*)").unwrap();
@@ -43,18 +44,37 @@ fn read_http_response(mut stream: &mut TlsStream<TcpStream>, cookies: &mut HashM
             let caps = found_url_re.captures(&line).unwrap();
             frontier.add(caps["url"].to_string()).unwrap();
         }
+        if line.starts_with("Connection: close") {
+            close_connection = true;
+        }
         more_header = !line.eq("\r\n");
-        print!("{}", line);
+        // print!("{}", line);
     }
 
     let mut body = vec![0u8; content_length];
     reader.read_exact(&mut body).unwrap();
+    // println!("{}", from_utf8(&body).unwrap());
+
+    if close_connection {
+        *stream = setup_connection();
+    }
 
     from_utf8(&body).unwrap().to_owned()
 }
 
 fn get_url(url: &str, stream: &mut TlsStream<TcpStream>, cookies: &mut HashMap<String, String>, frontier: &mut Queue<String>) -> String {
-    stream.write_all(format!("GET {}/ HTTP/1.1\r\nHost: fakebook.3700.network\r\n\r\n", url).as_bytes()).unwrap();
+    let mut cookie_header = String::new();
+    cookie_header.push_str("\r\nCookie: ");
+    for (name, value) in cookies.clone() {
+        cookie_header.push_str(&name);
+        cookie_header.push_str("=");
+        cookie_header.push_str(&value);
+        cookie_header.push_str("; ");
+    }
+    // println!("{}", cookie_header);
+    let get_message = format!("GET {} HTTP/1.1\r\nHost: fakebook.3700.network:443{}\r\n\r\n", url, cookie_header);
+    // println!("{}", get_message);
+    stream.write_all(get_message.as_bytes()).unwrap();
 
     read_http_response(stream, cookies, frontier)
 }
@@ -69,7 +89,8 @@ fn login(username: &str, password: &str, csrf_token: &str, stream: &mut TlsStrea
         cookie_header.push_str(&value);
     }
     let content_length = content.len();
-    let post_message = format!("POST /accounts/login/?next=/fakebook/ HTTP/1.1\r\nHost: fakebook.3700.network\r\nContent-Type: application/x-www-form-urlencoded\r\nContent-Length: {}{}\r\n\r\n{}", content_length, cookie_header, content);
+    let post_message = format!("POST /accounts/login/?next=/fakebook/ HTTP/1.1\r\nHost: fakebook.3700.network:443\r\nContent-Type: application/x-www-form-urlencoded\r\nContent-Length: {}{}\r\n\r\n{}", content_length, cookie_header, content);
+    // println!("{}", post_message);
     stream.write_all(post_message.as_bytes()).unwrap();
 
     read_http_response(stream, cookies, frontier)
@@ -83,10 +104,29 @@ fn get_links_from_html(html: &str) -> HashSet<String> {
         .collect::<HashSet<String>>()
 }
 
-fn parse_html_page(html: String, frontier: &mut Queue<String>) {
+fn get_secret_flag(html: &str) -> bool {
+    for secret_flag in Document::from(html).find(Class("secret_flag")).filter_map(|n| Some(n.text())) {
+        println!("{}", secret_flag);
+        return true;
+    }
+    return false;
+}
+
+fn parse_html_page(html: String, frontier: &mut Queue<String>)  -> bool {
     for url in get_links_from_html(&html) {
         frontier.add(url).unwrap();
     }
+    get_secret_flag(&html)
+}
+
+fn setup_connection() -> TlsStream<TcpStream> {
+    let hostname = "fakebook.3700.network";
+    let port = 443;
+
+    // Setup connection
+    let connector = TlsConnector::new().unwrap();
+    let stream = TcpStream::connect((hostname, port)).unwrap();
+    connector.connect(hostname, stream).unwrap()
 }
 
 fn main() {
@@ -95,15 +135,11 @@ fn main() {
     let password = &args[2];
     println!("Username: {}", username);
     println!("Password: {}", password);
-    let hostname = "fakebook.3700.network";
-    let port = 443;
-
-    // Setup connection
-    let connector = TlsConnector::new().unwrap();
-    let stream = TcpStream::connect((hostname, port)).unwrap();
-    let mut stream = connector.connect(hostname, stream).unwrap();
+    
+    let mut stream = setup_connection();
     let mut cookies = HashMap::<String, String>::new();
     let mut frontier = queue![];
+    let mut num_secret_flags_found = 0;
 
     let login_page = get_url("/accounts/login/?next=/fakebook/", &mut stream, &mut cookies, &mut frontier);
 
@@ -112,18 +148,21 @@ fn main() {
     
     // Post login request
     let _login_response = login(username, password, &csrf_token, &mut stream, &mut cookies, &mut frontier);
-    // println!("{}", login_response);
+    // println!("login: {}", login_response);
 
     let mut visited_pages = HashSet::<String>::new();
-    frontier.add("/fakebook".to_string()).unwrap();
+    frontier.add("/fakebook/".to_string()).unwrap();
+    frontier.remove().unwrap();
 
-    while frontier.size() > 0 {
+    while frontier.size() > 0 && num_secret_flags_found < 5 {
         let url = frontier.remove().unwrap();
-        if !visited_pages.contains(&url) && url.starts_with("/") {
-            println!("visiting {}", url);
+        if !visited_pages.contains(&url) && url.starts_with("/") && !url.starts_with("/accounts/logout/") {
+            // println!("visiting {}", url);
             let page = get_url(&url, &mut stream, &mut cookies, &mut frontier);
             visited_pages.insert(url);
-            parse_html_page(page, &mut frontier);
+            if parse_html_page(page, &mut frontier) {
+                num_secret_flags_found += 1;
+            }
         }
     }
 }
